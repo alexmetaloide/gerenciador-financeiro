a49890f4333ce7d011a7e0a38a7978ccca7390b7
ALEX <alexmetaloide@gmail.com>
2025-12-28 17:29:21 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gerenciador-financeiro/sw.js', { scope: '/gerenciador-financeiro/' })})}
